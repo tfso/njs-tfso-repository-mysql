7c32b5972673e066c0f4893d4d15ae51d567cdac
lostfields <ng@24sevenoffice.com>
2017-05-19 14:06:22 +0200

    moving some typings to @types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,1 @@
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/mssql/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/mysql/index.d.ts" />
