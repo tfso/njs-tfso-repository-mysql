2bce6477a0baead083e5b9baec464bc944f9715a
24SevenOffice <post@24sevenoffice.com>
2017-09-14 12:28:21 +0000

    Adding release files

---
+++ lib/db/querystream.d.ts
@@ -0,0 +1,15 @@
+import * as MySql from 'mysql';
+import { Query } from 'tfso-repository/lib/repository/db/query';
+import { IRecordSet, RecordSet } from 'tfso-repository/lib/repository/db/recordset';
+export declare abstract class QueryStream<TEntity> extends Query<TEntity> {
+    private _connection;
+    constructor(connection?: MySql.Connection);
+    connection: MySql.Connection;
+    protected input(name: string, value: any): void;
+    protected input(name: string, type: any, value: any): void;
+    protected createConnection(): MySql.Connection;
+    protected executeQuery(): Promise<IRecordSet<TEntity>>;
+    protected abstract transform(record: any): TEntity;
+}
+export default QueryStream;
+export { RecordSet };
---
+++ lib/test/base/connectionmock.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="node" />
+import * as MySql from 'mysql';
+import { EventEmitter } from 'events';
+export declare class ConnectionMock extends EventEmitter implements MySql.Connection {
+    private data;
+    private shouldFail;
+    private _config;
+    constructor(data: Array<any>, shouldFail?: boolean);
+    config: MySql.ConnectionOptions;
+    readonly threadId: number;
+    beginTransaction(callback: (err: MySql.QueryError | null) => void): void;
+    connect(callback?: (err: MySql.QueryError | null) => void): void;
+    commit(callback?: (err: MySql.QueryError | null) => void): void;
+    changeUser(options: MySql.ConnectionOptions, callback?: (err: MySql.QueryError | null) => void): void;
+    query<T extends MySql.RowDataPacket[][] | MySql.RowDataPacket[] | MySql.OkPacket | MySql.OkPacket[]>(sql: string, callback?: (err: MySql.QueryError | null, result: T, fields: MySql.FieldPacket[]) => any): MySql.Query;
+    query<T extends MySql.RowDataPacket[][] | MySql.RowDataPacket[] | MySql.OkPacket | MySql.OkPacket[]>(sql: string, values: any | any[] | {
+        [param: string]: any;
+    }, callback?: (err: MySql.QueryError | null, result: T, fields: MySql.FieldPacket[]) => any): MySql.Query;
+    query<T extends MySql.RowDataPacket[][] | MySql.RowDataPacket[] | MySql.OkPacket | MySql.OkPacket[]>(options: MySql.QueryOptions, callback?: (err: MySql.QueryError | null, result: T, fields?: MySql.FieldPacket[]) => any): MySql.Query;
+    query<T extends MySql.RowDataPacket[][] | MySql.RowDataPacket[] | MySql.OkPacket | MySql.OkPacket[]>(options: MySql.QueryOptions, values: any | any[] | {
+        [param: string]: any;
+    }, callback?: (err: MySql.QueryError | null, result: T, fields: MySql.FieldPacket[]) => any): MySql.Query;
+    end(callback?: (err: MySql.QueryError | null) => void): void;
+    end(options: any, callback?: (err: MySql.QueryError | null) => void): void;
+    destroy(): void;
+    pause(): void;
+    resume(): void;
+    escape(value: any): string;
+    escapeId(value: string): string;
+    escapeId(values: string[]): string;
+    format(sql: string, values?: any | any[] | {
+        [param: string]: any;
+    }): string;
+    rollback(callback: () => void): void;
+}
