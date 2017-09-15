50ea1fc6932ee3570ce2012a0c03103b9abce31a
24SevenOffice <post@24sevenoffice.com>
2017-09-15 05:58:47 +0000

    Adding release files

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import { Query } from 'tfso-repository/lib/repository/db/query';
+export { default as QueryRecordSet } from './db/queryrecordset';
+export { default as QueryStream } from './db/querystream';
+export { Query };
+export { default as Connection } from './db/connection';
